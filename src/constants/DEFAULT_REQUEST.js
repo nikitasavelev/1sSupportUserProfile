8151e9fd34e93c157b0f28868d3eb223aeefc552
Nikita Vozisov <Nikita_Vozisov@epam.com>
2019-04-17 09:29:18 +0300

    [add] refreshing token

--- src/constants/DEFAULT_REQUEST.js
+++ src/constants/DEFAULT_REQUEST.js
@@ -1,4 +1,6 @@
 import Store from "Store/store.js";
+import Router from "../router/router";
+import LoginService from "Services/LoginService";
 
 export const defaultRequestOptions = Object.freeze({
   headers: {
@@ -15,6 +17,12 @@ export const defaultRequestOptions = Object.freeze({
  * @param {function} modifyDataCallback if data needs to be modified after getting response
  */
 export async function requestToAPI(url, requestOptions = defaultRequestOptions, modifyDataCallback) {
+  if (isAccessTokenExpired()) {
+    const response = await LoginService.getNewAccessToken();
+    if (response.code === "Ok"){
+      updateStateAndLocalStorage(response.data);
+    }
+  }
   requestOptions.headers.Authorization = "Bearer " + Store.getters.getAuthorizationToken;
   return fetch(url, requestOptions)
     .then(res => res.json())
@@ -24,4 +32,28 @@ export async function requestToAPI(url, requestOptions = defaultRequestOptions, 
       }
       return res.data;
     });
+}
+
+function updateStateAndLocalStorage(response) {
+  if (response.expires && response.accessToken && response.refreshToken && response.role) {
+    localStorage.setItem("expires", response.expires);
+    localStorage.setItem("token", response.accessToken);
+    localStorage.setItem("refreshToken", response.refreshToken);
+    localStorage.setItem("role", response.role);
+
+    Store.dispatch("updateAuthorizationToken", response.accessToken);
+    Store.dispatch("updateRole", response.role);
+  }
+}
+
+// if "expires" is presented in localStorage, then
+// check if token is expired, redirect to loginPage otherwise
+function isAccessTokenExpired() {
+  if (localStorage.getItem("expires")) {
+    const expirationDate = Number(localStorage.getItem("expires"));
+    const currentDate = new Date().valueOf();
+    return !(expirationDate > currentDate);
+  } else {
+    Router.push({ name: "LoginPage" });
+  }
 }
