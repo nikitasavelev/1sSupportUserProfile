6ca41cd9f89a39452c950fb1e9b75beeb6f3e49b
Nikita Vozisov <Nikita_Vozisov@epam.com>
2019-06-09 19:17:35 +0300

    [refactor] calculateAnalytics

--- src/constants/COMMON_METHODS.js
+++ src/constants/COMMON_METHODS.js
@@ -5,154 +5,54 @@ export default function formatDate(UTCDate) {
     if (String(clonedValue).length === 1) {
       clonedValue = `0${clonedValue}`;
     }
-    return clonedValue
+    return clonedValue;
   }
   const day = addZeroPadding(date.getDate());
-  const month = addZeroPadding(date.getMonth()+1);
+  const month = addZeroPadding(date.getMonth() + 1);
   return `${day}.${month}.${date.getFullYear()}`;
-}
-
-export function assignVariables(
-  operator,
-  sumFromMango,
-  sumFromSystem,
-  averageCallsDuration,
-  maxCallDuration,
-  averageOnlineTimePerDay,
-  allCallsCount,
-  resolvedQuestionsCount,
-  callsOutcomeCount,
-  callsIncomeCount,
-  averageMark,
-  //specifically for graphs
-  unitsAmount,
-  twosAmount,
-  threesAmount,
-  foursAmount,
-  fivesAmount,
-  //specifically for operator
-  resolvedByClient,
-  resolvedByOperator
-) {
-  operator.calculatedKPI = {};
-  operator.calculatedKPI.questions = {};
-  operator.calculatedKPI.questions.createdCounts = {};
-  operator.calculatedKPI.questions.resolvedCounts = {};
-  operator.calculatedKPI.questions.marks = {};
-  operator.calculatedKPI.questions.marks.counts = {};
-  operator.calculatedKPI.calls = {};
-  operator.calculatedKPI.calls.durations = {};
-  operator.calculatedKPI.calls.counts = {};
-
-  operator.calculatedKPI.questions.createdCounts.fromMango = sumFromMango;
-  operator.calculatedKPI.questions.createdCounts.fromSystem = sumFromSystem;
-  operator.calculatedKPI.calls.durations.averageInSeconds = averageCallsDuration;
-  operator.calculatedKPI.calls.durations.maxInSeconds = maxCallDuration;
-  operator.calculatedKPI.calls.durations.onLineAverageInSeconds = averageOnlineTimePerDay;
-  operator.calculatedKPI.questions.resolvedCounts.total = resolvedQuestionsCount;
-  operator.calculatedKPI.calls.counts.total = allCallsCount;
-  operator.calculatedKPI.calls.counts.outcomes = callsOutcomeCount;
-  operator.calculatedKPI.calls.counts.incomes = callsIncomeCount;
-  operator.calculatedKPI.questions.marks.average = averageMark;
-
-  //specifically for graphs
-  operator.calculatedKPI.questions.marks.counts.units = unitsAmount;
-  operator.calculatedKPI.questions.marks.counts.twos = twosAmount;
-  operator.calculatedKPI.questions.marks.counts.threes = threesAmount;
-  operator.calculatedKPI.questions.marks.counts.fours = foursAmount;
-  operator.calculatedKPI.questions.marks.counts.fives = fivesAmount;
-  //specifically for operator
-  operator.calculatedKPI.questions.resolvedCounts.byClient = resolvedByClient;
-  operator.calculatedKPI.questions.resolvedCounts.byOperator = resolvedByOperator;
 }
 
 export function calculateAnalytics(operatorsAnalytics) {
   operatorsAnalytics.forEach(operator => {
-    let sumFromMango = 0;
-    let sumFromSystem = 0;
-    let averageCallsDuration = 0;
-    let maxCallDuration = 0;
-    let averageOnlineTimePerDay = 0;
-    let allCallsCount = 0;
-    let resolvedQuestionsCount = 0;
-    let callsOutcomeCount = 0;
-    let callsIncomeCount = 0;
-    let averageMark = 0;
-    let marksSum = 0;
-    let daysAmount = operator.kpi.length;
-
-    // specifically for graphs
-    let unitsAmount = 0;
-    let twosAmount = 0;
-    let threesAmount = 0;
-    let foursAmount = 0;
-    let fivesAmount = 0;
-
-    // specifically for operator
-    let resolvedByClient = 0;
-    let resolvedByOperator = 0;
+    let statistics = {
+      sumFromMango: 0,
+      sumFromSystem: 0,
+      averageCallsDuration: 0,
+      maxCallDuration: 0,
+      averageOnlineTimePerDay: 0,
+      allCallsCount: 0,
+      resolvedQuestionsCount: 0,
+      callsOutcomeCount: 0,
+      callsIncomeCount: 0,
+      averageMark: 0,
+      marksSum: 0,
+      daysAmount: operator.kpi.length,
+      // specifically for graphs
+      unitsAmount: 0,
+      twosAmount: 0,
+      threesAmount: 0,
+      foursAmount: 0,
+      fivesAmount: 0,
+      // specifically for operator
+      resolvedByClient: 0,
+      resolvedByOperator: 0
+    };
 
     operator.kpi.forEach(kpi => {
-      sumFromMango += kpi.questions.createdCounts.fromMango;
-      sumFromSystem += kpi.questions.createdCounts.fromSystem;
-      averageCallsDuration += kpi.calls.counts.total * kpi.calls.durations.averageInSeconds;
-      if (kpi.calls.durations.maxInSeconds > maxCallDuration) {
-        maxCallDuration = kpi.calls.durations.maxInSeconds;
+      increaseStatsValues(statistics, kpi);
+      statistics.averageCallsDuration += kpi.calls.counts.total * kpi.calls.durations.averageInSeconds;
+      if (kpi.calls.durations.maxInSeconds > statistics.maxCallDuration) {
+        statistics.maxCallDuration = kpi.calls.durations.maxInSeconds;
       }
-      averageOnlineTimePerDay += kpi.calls.durations.onLineAverageInSeconds;
-      resolvedQuestionsCount += kpi.questions.resolvedCounts.total;
-      allCallsCount += kpi.calls.counts.total;
-      callsOutcomeCount += kpi.calls.counts.outcomes;
-      callsIncomeCount += kpi.calls.counts.incomes;
-      marksSum += Object.values(kpi.questions.marks.counts).reduce((sum, count) => sum + count);
-      averageMark +=
-        kpi.questions.marks.counts.units +
-        kpi.questions.marks.counts.twos * 2 +
-        kpi.questions.marks.counts.threes * 3 +
-        kpi.questions.marks.counts.fours * 4 +
-        kpi.questions.marks.counts.fives * 5;
-
-      // specifically for graphs
-      unitsAmount += kpi.questions.marks.counts.units;
-      twosAmount += kpi.questions.marks.counts.twos;
-      threesAmount += kpi.questions.marks.counts.threes;
-      foursAmount += kpi.questions.marks.counts.fours;
-      fivesAmount += kpi.questions.marks.counts.fives;
-
-      // specifically for operator
-      resolvedByClient += kpi.questions.resolvedCounts.byClient;
-      resolvedByOperator += kpi.questions.resolvedCounts.byOperator;
+      statistics.marksSum += Object.values(kpi.questions.marks.counts).reduce((sum, count) => sum + count);
+      statistics.averageMark += Object.values(kpi.questions.marks.counts).reduce((sum, count, index) => {
+        return sum + count * (index + 1);
+      });
     });
-    // averages
-    averageCallsDuration = allCallsCount === 0 ? 0 : averageCallsDuration / allCallsCount;
-    averageOnlineTimePerDay = daysAmount - 1 === 0 ? 0 : averageOnlineTimePerDay / (daysAmount - 1);
-    averageMark = marksSum === 0 ? 0 : averageMark / marksSum;
-    // assignments
-    assignVariables(
-      operator,
-      sumFromMango,
-      sumFromSystem,
-      averageCallsDuration,
-      maxCallDuration,
-      averageOnlineTimePerDay,
-      allCallsCount,
-      resolvedQuestionsCount,
-      callsOutcomeCount,
-      callsIncomeCount,
-      averageMark,
-      //specifically for graphs
-      unitsAmount,
-      twosAmount,
-      threesAmount,
-      foursAmount,
-      fivesAmount,
-      //specifically for operator
-      resolvedByClient,
-      resolvedByOperator
-    );
+    calculateAverages(statistics);
+    assignVariables(operator, statistics);
   });
 }
-
 export function setLocalStorageValues(namesArray, valuesObject) {
   namesArray.forEach(name => {
     localStorage.setItem(name, valuesObject[name]);
@@ -165,16 +65,89 @@ export function removeLocalStorageValues(namesArray) {
   });
 }
 
-export function fractionalHoursToIntegerHoursAndMinutes(fractionalHours){
+export function fractionalHoursToIntegerHoursAndMinutes(fractionalHours) {
   const hours = Math.trunc(fractionalHours);
   let minutes = ((fractionalHours - hours) * 60).toFixed();
   if (minutes.length === 1) minutes = `0${minutes}`;
   return `${hours}:${minutes}`;
 }
 
-
 export const toDateString = (fromDate, toDate) => {
-  fromDate = fromDate.split('.').reverse().join('-');
-  toDate = toDate.split('.').reverse().join('-');
-  return [fromDate, toDate]
+  fromDate = fromDate
+    .split(".")
+    .reverse()
+    .join("-");
+  toDate = toDate
+    .split(".")
+    .reverse()
+    .join("-");
+  return [fromDate, toDate];
+};
+
+// non-export functions
+function assignVariables(operator, statistics) {
+  operator.calculatedKPI = {
+    questions: {
+      createdCounts: {
+        fromMango: statistics.sumFromMango,
+        fromSystem: statistics.sumFromSystem
+      },
+      resolvedCounts: {
+        total: statistics.resolvedQuestionsCount,
+        //specifically for operator
+        byClient: statistics.resolvedByClient,
+        byOperator: statistics.resolvedByOperator
+      },
+      marks: {
+        average: statistics.averageMark,
+        counts: {
+          //specifically for graphs
+          units: statistics.unitsAmount,
+          twos: statistics.twosAmount,
+          threes: statistics.threesAmount,
+          fours: statistics.foursAmount,
+          fives: statistics.fivesAmount
+        }
+      }
+    },
+    calls: {
+      durations: {
+        averageInSeconds: statistics.averageCallsDuration,
+        maxInSeconds: statistics.maxCallDuration,
+        onLineAverageInSeconds: statistics.averageOnlineTimePerDay
+      },
+      counts: {
+        incomes: statistics.callsIncomeCount,
+        outcomes: statistics.callsOutcomeCount,
+        total: statistics.allCallsCount
+      }
+    }
+  };
+}
+
+function increaseStatsValues(statistics, kpi) {
+  statistics.sumFromMango += kpi.questions.createdCounts.fromMango;
+  statistics.sumFromSystem += kpi.questions.createdCounts.fromSystem;
+  statistics.averageOnlineTimePerDay += kpi.calls.durations.onLineAverageInSeconds;
+  statistics.resolvedQuestionsCount += kpi.questions.resolvedCounts.total;
+  statistics.allCallsCount += kpi.calls.counts.total;
+  statistics.callsOutcomeCount += kpi.calls.counts.outcomes;
+  statistics.callsIncomeCount += kpi.calls.counts.incomes;
+  // specifically for graphs
+  statistics.unitsAmount += kpi.questions.marks.counts.units;
+  statistics.twosAmount += kpi.questions.marks.counts.twos;
+  statistics.threesAmount += kpi.questions.marks.counts.threes;
+  statistics.foursAmount += kpi.questions.marks.counts.fours;
+  statistics.fivesAmount += kpi.questions.marks.counts.fives;
+  // specifically for operator
+  statistics.resolvedByClient += kpi.questions.resolvedCounts.byClient;
+  statistics.resolvedByOperator += kpi.questions.resolvedCounts.byOperator;
+}
+
+function calculateAverages(statistics) {
+  statistics.averageCallsDuration =
+    statistics.allCallsCount === 0 ? 0 : statistics.averageCallsDuration / statistics.allCallsCount;
+  statistics.averageOnlineTimePerDay =
+    statistics.daysAmount - 1 === 0 ? 0 : statistics.averageOnlineTimePerDay / (statistics.daysAmount - 1);
+  statistics.averageMark = statistics.marksSum === 0 ? 0 : statistics.averageMark / statistics.marksSum;
 }
